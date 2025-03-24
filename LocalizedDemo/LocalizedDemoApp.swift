69ef4de25b2235603eb46d3d5af4f60d06970fd9
rkonkala41 <RKonkala41@massmutual.com>
2025-03-24 16:35:21 -0400

    Initial Commit

---
+++ LocalizedDemo/LocalizedDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocalizedDemoApp.swift
+//  LocalizedDemo
+//
+//  Created by Ratheesh Reddy on 3/24/25.
+//
+
+import SwiftUI
+
+@main
+struct LocalizedDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
